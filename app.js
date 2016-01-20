60da4616f21223acd8557143d7901844f9a1a016
Michael Bushoy <mbushoy@gmail.com>
2016-01-20 09:51:26 -0500

    chchchanges

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
 var express=require("express");
+var app=express();
 var morgan=require("morgan");
 var fs=require("fs");
 var swig=require("swig");
 var tweetBank=require("./tweetBank");
-var bodyParser=require("body-parser");
-var urlEncodedParsing=bodyParser.urlencoded({ extended: false });
-var jsonParsing=bodyParser.json();
-
-var socketio = require('socket.io');
-// ...
-
-swig.setDefaults({cache:false});
-var app=express();
 var routes = require('./routes/');
-app.use( '/', routes(io) );
-
-app.use(express.static('public'));
-
-app.listen(1337,function(){console.log("hi");});
-var server = app.listen(1338);
+var socketio = require('socket.io');
+var bodyParser = require('body-parser');
 var io = socketio.listen(server);
+var server = app.listen(1337,  function(req, res, next){
+  console.log("Server listening");
+});
 
+//app uses
+app.use('/',routes(io));
+app.use(express.static('public'));
+app.use(morgan('combined', {stream: accessLogStream}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use(function(req, res, next){
+//     console.log(req.method + " " + req.url + " " + res.statusCode);
+//     next();
+// });
 
+//view engine
+swig.setDefaults({cache:false});
 app.engine("html",swig.renderFile);
-
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 // create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
 
+// catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
 // setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
-app.use(bodyParser.text({ type: 'text/html' }));
-
-var userRouter = express.Router();
-app.use('/users', userRouter);
-
-userRouter.get('/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {"name": name} );
-  res.render( 'index', { tweets: list, showForm:true, thisName: name } );
-});
-
-var mainRouter=express.Router();
-app.use('/', mainRouter);
-
-mainRouter.post("/tweets",urlEncodedParsing,function(req,res) {
-   var name = req.body.name;
-   var text = req.body.text;
-   tweetBank.add(name, text);
-   io.sockets.emit('new_tweet', { "name": name, "text": text });
-   res.redirect("/");
-});
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,41 @@
+
 module.exports = function (io) {
+  var express = require('express');
+  var router = express.Router();
+  // could use one line instead: var router = require('express').Router();
+  var tweetBank = require('../tweetBank');
+  var bodyParser = require('body-parser');
 
-var express = require('express');
-var router = express.Router();
-// could use one line instead: var router = require('express').Router();
-var tweetBank = require('../tweetBank');
+	// create application/json parser
+	var jsonParser = bodyParser.json();
+	// create application/x-www-form-urlencoded parser
+	var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
-});
+  router.get('/', function (req, res) {
+    var tweets = tweetBank.list();
+    res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+  });
 
-return router;
-}
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find( {name: name} );
+    res.render( 'index', { title: 'Twitter.js - Posts by '+ name, tweets: list, showForm: true } );
+  });
+
+  router.get('/users/:name/tweets/:id', function(req, res) {
+    var name = req.params.name;
+    var id = parseInt(req.params.id);
+    var list = tweetBank.find( {name: name, id: id} );
+    res.render( 'index', { title: 'Twitter.js - Tweet number ' + id + ' by '+ name, tweets: list, showForm: false } );
+  });
+
+  router.post('/submit', urlencodedParser, function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    var newTweet = tweetBank.add(name, text);
+    io.sockets.emit('new_tweet', newTweet);
+    res.redirect('/');
+  });
+
+  return router;
+};
